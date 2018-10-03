4ed8404c94a0354a853e8b775b8b48d318fdaecc
bert2 <shuairan@gmail.com>
2018-10-03 10:22:33 +0200

    setup db context

---
+++ GarbageDb/Blog.cs
@@ -0,0 +1,13 @@
+﻿namespace GarbageDb
+{
+    using System.Collections.Generic;
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; } = new HashSet<Post>();
+    }
+}
---
+++ GarbageDb/BloggingContext.cs
@@ -0,0 +1,39 @@
+﻿namespace GarbageDb
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext()
+        {
+        }
+
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blog> Blog { get; set; }
+
+        public virtual DbSet<Post> Post { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=blogging.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>(x => x
+                .Property(e => e.Url).IsRequired());
+
+            modelBuilder.Entity<Post>(x => x
+                .HasOne(d => d.Blog)
+                .WithMany(p => p.Post)
+                .HasForeignKey(d => d.BlogId));
+        }
+    }
+}
